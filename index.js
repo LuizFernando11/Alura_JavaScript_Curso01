3ebf84ab1c3e0e0cfb5b47f614bddbdd3515840f
LuizFernando11 <luiz.fernando@viannasempre.com.br>
2025-03-09 22:11:04 -0300

    Aula 2.2
    Ouvinte de eventos

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const inputItem = document.getElementById('input-Item')
+const inputItem = document.getElementById("input-item");
+
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    console.log(inputItem.value);
+})
